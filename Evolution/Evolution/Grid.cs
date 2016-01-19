89068e86684c0290c76b522d62ebbca4764ffe0b
Brandon Richardson <Brandon.Richardson@extendhealth.com>
2016-01-19 15:38:28 -0700

    Added World, Grid, GridUnit, and generating food per GridUnit

---
+++ Evolution/Evolution/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution
+{
+    public class Grid
+    {
+        private GridUnit[,] grid { get; set; }
+        private int dimensionSize { get; set; }
+
+        public Grid(int gridUnitCount)
+        {
+            dimensionSize = (int)Math.Pow(gridUnitCount, .5);
+            grid = new GridUnit[dimensionSize, dimensionSize];
+        }
+
+        public GridUnit GetUnitAt(int x, int y)
+        {
+            return grid[y, x];
+        }
+
+    }
+}
---
+++ Evolution/Evolution/World.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace Evolution
+{
+    public static class World
+    {
+        // World is a square, so all lengths are for both X and Y
+        private static int worldSize { get; set; }
+        private static int foodCount { get; set; }
+        private static int avgFoodPerGridUnit { get; set; }
+        private static int gridUnitCount { get; set; }
+        private static int gridUnitSize { get; set; }
+        public static Grid grid { get; set; } 
+        
+        static World()
+        {
+            SetWorldProperties();
+            GenerateWorld();
+        }
+
+        private static void SetWorldProperties()
+        {
+            worldSize = 100;
+            foodCount = 500;
+            avgFoodPerGridUnit = foodCount / worldSize;
+            gridUnitCount = (int) Math.Pow(worldSize, .5);
+            gridUnitSize = worldSize/gridUnitCount; 
+            var unitsPerDimension = (int) Math.Pow(gridUnitCount, .5);
+            grid = new Grid(gridUnitCount);
+        }
+
+        private static void GenerateWorld()
+        {
+            
+        }
+
+        private static void GenerateFood()
+        {
+            var remainingFood = foodCount;
+
+            for (var y = 0; y < gridUnitSize; y++)
+                for (var x = 0; x < gridUnitSize; x++)
+                {
+                    var maxFood = remainingFood/4;
+                    var medianFood = remainingFood/(gridUnitCount - x - 1);
+                    grid.GetUnitAt(x, y).food = GetWeightedRandomNum(medianFood);
+                }
+        }
+
+        private static int GetWeightedRandomNum(int median)
+        {
+            var numList = new List<int>();
+
+            for (; median > 0; median--)
+            {
+                for (var i = 0; i < median; i++)
+                {
+                    numList.Add(median + i);
+                    numList.Add(median - i);
+                }
+            }
+
+            return numList.ElementAt(new Random().Next(0, numList.Count - 1));
+        }
+
+        private static int GetWeightCount(int medianFood, int i)
+        {
+            return (medianFood - i);
+        }
+
+        public static void Run()
+        {
+        }
+    }
+}
